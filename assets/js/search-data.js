b9ba5f6e5257a6dc3f355d67a197b2cdcd23059d
tyro-das <45478008+tyro-das@users.noreply.github.com>
2026-03-01 09:52:37 +0000

    Deploying to gh-pages from @ tyro-das/tyro-das.github.io@32adcb130ca6ff46ef3bee2e3901b2a0040cdf24 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,13 +30,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "nav-repositories",
-          title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/repositories/";
-          },
         },{id: "nav-cv",
           title: "CV",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
@@ -490,13 +483,6 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("mailto:%70%75%73%68%70%61%6B%64%61%73@%6E%69%74%6D.%61%63.%69%6E", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/", "_blank");
         },
       },{
         id: 'social-linkedin',
